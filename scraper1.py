2a051964eeeed9e681aecb9a63edc2a368186aef
FelixHertz <84127057+FelixHertz@users.noreply.github.com>
2022-10-07 15:21:41 +0200

    Add files via upload

---
+++ scraper1.py
@@ -0,0 +1,78 @@
+from flask import Flask, render_template, request, flash
+import time
+from datetime import datetime
+import base64
+import os.path
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import os
+import mysql.connector
+
+mydb = mysql.connector.connect(host="sql11.freesqldatabase.com", user="sql11524700", passwd="tY17NVeXtN", database="sql11524700")
+mycursor = mydb.cursor()
+
+def add_data(wort, gebrauch):
+	try:
+		sql = "INSERT INTO wort_erg (wort, gebrauch) VALUES (%s, %s)"
+		val = (wort, gebrauch)
+		mycursor.execute(sql, val)
+		mydb.commit()
+	except:
+		continue
+	
+
+def add_data_other(wort, gebrauch):
+	try:
+		sql = "INSERT INTO other (wort, gebrauch) VALUES (%s, %s)"
+		val = (wort, gebrauch)
+		mycursor.execute(sql, val)
+		mydb.commit()
+	except:
+		continue
+	
+
+
+
+# Chrome Options
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument ("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+
+# Starting Web Driver
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),chrome_options=chrome_options)
+driver.get("http://www.google.com/")
+
+
+count = 0
+liste = ['test']
+
+with open('deutsch.txt') as fp:
+    contents = fp.readlines()
+
+
+def scrape():
+	for entry in contents:
+		try:
+			driver.get(f"https://www.duden.de/rechtschreibung/{entry}")
+			gebrauch = driver.find_element(By.CSS_SELECTOR, "body > div.dialog-off-canvas-main-canvas > div.tabloid > div.tabloid__sheet.tabloid__sheet--has-sidebar.tabloid__sheet--has-insert.tabloid__sheet--has-main.tabloid__sheet--has-main-top > main > article > dl:nth-child(5) > dd")
+			result = gebrauch.get_attribute("innerHTML")
+			liste.append(entry)
+			if result == 'bildungssprachlich':
+				add_data(entry, 'bildungssprachlich')
+			if result == 'gehoben':
+				add_data(entry, 'gehoben')
+			if result != 'bildungssprachlich':
+				if result != 'gehoben':
+					add_data_other(entry, result)
+
+		except:
+			continue
+		
+	
+scrape()
