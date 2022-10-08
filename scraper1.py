ebfb9aa6e7dc8d933aa66a72196244b837127c80
FelixHertz <84127057+FelixHertz@users.noreply.github.com>
2022-10-08 10:48:46 +0200

    Add files via upload

--- scraper1.py
+++ scraper1.py
@@ -43,8 +43,8 @@ chrome_options.add_argument ("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
 
-# Starting Web Driver
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 driver.get("http://www.google.com/")
 
 
